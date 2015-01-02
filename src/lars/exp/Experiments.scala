aa50f7c39bdd90aa17becf71ea686721b1ecf91d
hbeck <harald.beck@gmx.at>
2015-01-02 21:33:42 +0100

    diamond, box

--- src/lars/exp/Experiments.scala
+++ src/lars/exp/Experiments.scala
@@ -1,8 +1,9 @@
 package lars.exp
 
 import lars.exp.Formulas._
+import lars.exp.Streams._
 
-import scala.collection.immutable.Set
+import scala.collection.immutable.{HashMap, HashSet, Set}
 
 /**
  * Created by hb on 1/2/15.
@@ -10,21 +11,33 @@ import scala.collection.immutable.Set
 object Experiments {
   def main(args: Array[String]) {
 
-    val a = Atom("a");
-    val b = Atom("b");
-    val c = Atom("c");
+    val T = Timeline(0,50)
+    val a36 = Atom("tram(a1,b)")
+    val a40 = Atom("tram(a3,h)")
+    val v36 = 36 -> Set(a36)
+    val v40 = 40 -> Set(a40)
+    val v = Evaluation(HashMap(v36,v40))
+    val D = Stream(T,v)
 
-    val fm1 = Or(And(a,b),Not(c))
-    val fm2 = (a.and(b)).or(c.not())
-    val fm3 = (a and b) or (c not)
-    val fm4 = (a and b) or not(c)
+    println(D)
 
-    val emptySet = Set[Atom]()
-    val I0 = Interpretation(emptySet)
+    val T0 = T
+    val a43 = Atom("exp(a3,m)")
+    val a44 = Atom("exp(a1,m)")
+    val v43 = 43 -> Set(a43)
+    val v44 = 44 -> Set(a44)
+    val v0 = Evaluation(v.map + (v43,v44))
+    val S0 = Stream(T0,v0)
 
-    for (fm <- List(fm1,fm2,fm3,fm4)) {
-      println("I0 |= " + fm + "?: " + (I0 |= fm))
-    }
+    println(S0)
+
+    val W = new HashSet[WindowOperator]
+    val B = new HashSet[Atom]
+    val M = Structure(T0,v0,W,B)
+
+    val q1 = M/S0/43 ||- a43
+
+    println("M,S*,43 ||- exp(a3,m) ?: "+q1)
 
   }
 }
