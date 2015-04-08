5e4ff5ab5914f1ce3bbd763fe6bb642ad4bc4f27
hbeck <harald.beck@gmx.at>
2015-04-08 16:58:57 +0200

    changed to sbt structure

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "lars"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+
---
+++ src/main/scala/lars/exp/Formulas.scala
@@ -0,0 +1,48 @@
+package lars.exp
+
+import lars.exp.WindowFunctions.WindowFunction
+import lars.exp.Streams.Stream
+
+/**
+ * Created by hb on 1/2/15.
+ */
+
+object Formulas {
+
+  abstract class Formula {
+    //binary ops
+    def and(fm: Formula): Formula = And(this,fm)
+    def or(fm: Formula): Formula = Or(this, fm)
+    def implies(fm: Formula): Formula = Impl(this, fm)
+    def not(): Formula = Not(this)
+  }
+
+  //unary ops
+  def not(fm: Formula) = Not(fm)
+  def b(fm: Formula) = B(fm)
+  def d(fm: Formula) = D(fm)
+  def at = (t:Int) => ((fm: Formula) => At(t,fm))
+  def win = (w:WindowFunction, ch: Choice, x: Seq[Int]) => ((fm: Formula) => Win(w,ch,x,fm))
+
+  case class Atom(s: String) extends Formula
+  case class Not(fm: Formula) extends Formula
+  case class And(fm1: Formula, fm2: Formula) extends Formula
+  case class Or(fm1: Formula, fm2: Formula) extends Formula
+  case class Impl(fm1: Formula, fm2: Formula) extends Formula
+  case class D(fm: Formula) extends Formula
+  case class B(fm: Formula) extends Formula
+  case class At(t: Int, fm: Formula) extends Formula
+  case class Win(wfn: WindowFunction, ch: Choice, x: Seq[Int], fm: Formula) extends Formula
+
+  abstract class Choice() extends Function2[Stream,Stream,Stream]
+
+  val ch1 = new Choice() {
+    override def apply(s1: Stream, s2: Stream) : Stream = s1
+  }
+
+  val ch2 = new Choice() {
+    override def apply(s1: Stream, s2: Stream) : Stream = s2
+  }
+
+}
+
