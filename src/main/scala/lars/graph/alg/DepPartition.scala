639d61cb9593bb8917702d9ed1ebfdf15dedb6d3
Edward Toth <e0725631@student.tuwien.ac.at>
2015-08-08 17:25:07 +0200

    code cleanup; node merging works now (it was needed for the 3rd testcase)

--- src/main/scala/lars/graph/alg/DepPartition.scala
+++ src/main/scala/lars/graph/alg/DepPartition.scala
@@ -18,17 +18,18 @@ case class DepPartition() extends (DepGraph => Map[ExtendedAtom,Set[ExtendedAtom
 
   var keyCnt = -1 //maximal partition Index
   var block = new mutable.HashMap[Int, Set[ExtendedAtom]]()
-  //TODO inBlock map node 2 boolean
   var partition = new mutable.HashMap[ExtendedAtom,Int]()
 
   override def apply(g: DepGraph): Map[ExtendedAtom, Set[ExtendedAtom]] = {
-    println(g.nodes)
     var result = new collection.immutable.HashMap[ExtendedAtom,Set[ExtendedAtom]]
+
 
     for (node <- g.nodes) {
       if (!inBlock(node)) {
         if (canAddNodes(g,node)) {
-          addNodes(node, g, findNeighbourBlock(g,node))
+/*          println("from graph: "+node)
+          println("neighbour to: "+neighbourBlock(g,node))*/
+          addNodes(node, g, neighbourBlock(g,node))
         }
       }
     }
@@ -43,8 +44,8 @@ case class DepPartition() extends (DepGraph => Map[ExtendedAtom,Set[ExtendedAtom
   }
 
   def hasIncoming(g: DepGraph, node: ExtendedAtom) : Boolean = {
-    for(n <- g.nodes){
-      if(g.adjList(n).contains(node)) return true
+    for (n <- g.nodes) {
+      if (g.adjList(n).contains(node)) return true
     }
     false
   }
@@ -66,183 +67,122 @@ case class DepPartition() extends (DepGraph => Map[ExtendedAtom,Set[ExtendedAtom
    * pre: node is not in a block yet
    */
   def addNodes(node: ExtendedAtom, g: DepGraph, nb: Option[(Int, Set[ExtendedAtom])]) : Unit = {
-    //TODO replace loop by findNeighbourBlock(g,node) (returns Option[Set[ExtendedAtom]])
-//    for ((key,value) <- block) {
-//      val b = findNeighbourBlock(g,node) //neighbourBlock(g,value,node)
-
       if (nb.isDefined && !hasSomePathWithGrt(g, nb.get._2, Set(node))) {
-        println("to existing")
+//        println("add "+node+" to existing blockSet")
         addNodeToBlock(g,nb.get._1,nb.get._2,node)
-/*        if (!hasSomePathWithGrt(g, b.get._2, Set(node))) {
-          block(b.get._1) += node
-          addNeighbours(g, node)
-          return
-        }*/
         return
       }
-    println("to new")
     keyCnt += 1
     block += (keyCnt -> Set())
     addNodeToBlock(g,keyCnt,block(keyCnt),node)
- //   addNeighbours(g,node)
   }
 
   def addNodeToBlock(g: DepGraph, blockIndex: Int, blockSet: Set[ExtendedAtom], node: ExtendedAtom) : Unit = {
-    println(block)
-    println(partition)
-    println(blockSet)
-    println(node)
-    println(hasSomePathWithGrt(g,blockSet,Set(node)))
-    println("---")
-   if (inBlock(node)) return
+/*    println("block: "+block)
+    println("partition: "+partition)
+    println("node: "+node)
+    println("hasGrtPath: "+hasSomePathWithGrt(g,blockSet,Set(node)))
+    println("---")*/
+   if (inBlock(node)) return // necessary?
 
      block(blockIndex) += node
      partition += (node -> blockIndex)
      addNeighbours(g, node)
   }
 
-  def findNeighbourBlock(g: DepGraph, node: ExtendedAtom) : Option[(Int,Set[ExtendedAtom])] = {
+  def neighbourBlock(g: DepGraph, node: ExtendedAtom) : Option[(Int,Set[ExtendedAtom])] = {
+//    println("\tblock: "+block)
     for ((key,value) <- block) {
-      val b = neighbourBlock(g,value,node)
-      if (b.isDefined) return Option((key,value))
+      for (v <- value) {
+//        println("outgoing: "+g.outgoing(v))
+        if (g.outgoing(v).contains(node)) return Option((key,value))
+      }
     }
    None
-  }
-
-
-  /*checks if node is a neighbour of any of the vertices in value
-  * @return value, if node is a neighbour to any element of value, null otherwise*/
-  def neighbourBlock(g: DepGraph, value: Set[ExtendedAtom], node: ExtendedAtom): Option[Set[ExtendedAtom]] = {
-    for (v <- value) {
-      if (g.outgoing(v).contains(node)) return Option(value)
-    }
-    None
-  }
-
-  /*checks if the set of r, which contains node, has any other elements in it
-  * @return true if there are no other elements but node, false otherwise*/
-  def isAloneInBlock(node: ExtendedAtom) : Boolean = {
-    if(partition.contains(node) && block(partition(node)).size == 1) {
-     block.remove(partition(node))
-     partition.remove(node)
-     return true
-    }
-/*    for ((key,value) <- block) {
-      if (value.contains(node) && value.size == 1) {
-        block.remove(key)
-        return true
-      }
-    }*/
-    false
   }
 
   /* checks if the neighbours of node are already in r, and adds them if not */
   def addNeighbours(g: DepGraph, node: ExtendedAtom) = {
     for (n <- g.adjList(node)) {
       if (!inBlock(n) || isAloneInBlock(n)) {
-//        addNodeToBlock(g,partition(node),block(partition(node)),n)
         addNodes(n,g,Option(partition(node),block(partition(node))))
-       // addNodes(n, g)
-      }/* else {
+      } else if (partition(node) != partition(n)) {
+        println("trying merge between "+partition(node)+" and "+partition(n))
         tryMerge(g,node,n)
-      }*/
+      }
     }
   }
 
+  /*checks if the set of block, which contains node, has any other elements in it
+  * @return true if there are no other elements but node, false otherwise*/
+  def isAloneInBlock(node: ExtendedAtom) : Boolean = {
+    if (partition.contains(node) && block(partition(node)).size == 1) {
+      block.remove(partition(node))
+      partition.remove(node)
+      return true
+    }
+    false
+  }
+
   def tryMerge(g: DepGraph, node: ExtendedAtom, n: ExtendedAtom) : Unit = {
-    println(block)
-    println("node: "+node+" n: "+n)
 
-    var fromSet, toSet:Set[ExtendedAtom] = null
-    var fromKey, toKey = -1
-    var grtP = false
+    val fromSet = block(partition(node))
+    val fromKey = partition(node)
 
-    for ((key,value) <- block) {
-      if (value.contains(node)) {
-        fromKey = key
-        fromSet = value
-      }
-      if (value.contains(n)) {
-        toKey = key
-        toSet = value
-      }
-    }
-    if (fromKey == toKey) return
-    println("fromK: "+fromKey+" toK: "+toKey)
-/*    for (from <- fromSet) {
-      for (to <- toSet) {
-        println("from: "+from+" to: "+to)
-        if (dfs(g,from,to) || dfs(g,to,from)) {
-          grtP = true
-        }
-      }
-    }*/
-    if (hasSomePathWithGrt(g,fromSet,toSet) || hasSomePathWithGrt(g,toSet,fromSet)) {
-      grtP = true
-    }
-    if (!grtP) merge(g,fromKey,toKey)
+    val toSet = block(partition(n))
+    val toKey = partition(n)
+
+    if (!hasSomePathWithGrt(g,fromSet,toSet) && !hasSomePathWithGrt(g,toSet,fromSet))
+      merge(g,fromKey,toKey)
   }
 
   def merge(g:DepGraph,fromKey: Int, toKey: Int) = {
     if (!hasGrtCycle(g,fromKey,toKey))
       block(math.min(fromKey,toKey)) ++= block(math.max(fromKey,toKey))
+    for (v <- block(math.max(fromKey,toKey))) {
+      partition(v) = math.min(fromKey,toKey)
+    }
     block.remove(math.max(fromKey,toKey))
+
   }
 
+  /* naive algorithm to check for cycles between the nodes of block */
   def hasGrtCycle(g:DepGraph, fromKey: Int, toKey: Int) : Boolean = {
     val tmp:Set[ExtendedAtom] = block(fromKey) ++ block(toKey)
-    var tToV, tToVG, vToT, vToTG = false
-    var newR = collection.mutable.HashMap[Int,Set[ExtendedAtom]]()
-    var cnt = 0
 
-    for ((key,value) <- block){
-      cnt = key
-      if(key != fromKey && key != toKey) {
-        newR += (key->value)
-      }
-    }
-    newR += (cnt+1->tmp)
-
-    /*Loop-condition-party*/
-    for((ktmp,tmp)<-newR) {
-      for (t <- tmp) {
-        for ((key, value) <- newR) {
-          if (ktmp != key /*&& key != fromKey && key != toKey*/) {
-            tToV = false
-            tToVG = false
-            vToT = false
-            vToTG = false
-            for (v <- value) {
-              if (g.hasEdge(t, v)) {
-                tToV = true
-                if (isGrt(g, t, v)) {
-                  tToVG = true
-                }
-              }
-              if (g.hasEdge(v, t)) {
-                vToT = true
-                if (isGrt(g, v, t)) {
-                  vToTG = true
-                }
-              }
-            }
-          }
-          if ((tToVG && vToT) || (vToTG && tToV)) return true
+    for ((ktmp,tmp) <- block) {
+      for ((key, value) <- block) {
+        if (ktmp != fromKey && key != toKey && ktmp != toKey && key != fromKey) {
+          val edges = hasEdges(g, tmp, value)
+          if (edges.nonEmpty && !hasGrtEdges(g, edges))
+            if (hasSomePathWithGrt(g, tmp, value) || hasSomePathWithGrt(g, value, tmp)) return true
         }
       }
     }
     false
   }
 
+  def hasEdges(g: DepGraph, tmp: Set[ExtendedAtom], value: Set[ExtendedAtom]): Set[(ExtendedAtom,ExtendedAtom)] = {
+    var edgeSet:Set[(ExtendedAtom,ExtendedAtom)] = Set()
+    for (t <- tmp) {
+      for (v <- value) {
+        if (g.hasEdge(t,v)) edgeSet = edgeSet ++ Set((t,v))
+        else if (g.hasEdge(v,t)) edgeSet = edgeSet ++ Set((v,t))
+      }
+    }
+    edgeSet
+  }
+
+  def hasGrtEdges(g: DepGraph, edges: Set[(ExtendedAtom, ExtendedAtom)]): Boolean = {
+    for ((from,to) <- edges) {
+      if (isGrt(g,from,to)) return true
+    }
+    false
+  }
 
   /* checks if a node has already been added to r */
   def inBlock(node: ExtendedAtom) : Boolean = {
-    if(partition.contains(node)) return true
-/*    for ((key,value) <- block) {
-      if (value.contains(node)) {
-        return true
-      }
-    }*/
+    if (partition.contains(node)) return true
     false
   }
 
