e2fbfc0c681221959c152f974d6f4b2c08e4333c
hbeck <harald.beck@gmx.at>
2015-08-27 12:35:20 +0200

    intermediate step towards a simpler algorithm for stream stratification

---
+++ src/main/scala/lars/graph/quotient/Block.scala
@@ -0,0 +1,8 @@
+package lars.graph.quotient
+
+/**
+ * Created by hb on 8/11/15.
+ */
+class Block {
+
+}
---
+++ src/main/scala/lars/strat/DepQuotientGraph.scala
@@ -0,0 +1,8 @@
+package lars.strat
+
+/**
+ * Created by hb on 8/11/15.
+ */
+class DepQuotientGraph {
+
+}
